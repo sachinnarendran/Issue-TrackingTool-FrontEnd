cac518b3576cb6d8c30578158b9d1ec339d225f5
Your Name <you@example.com>
2019-11-19 22:11:37 -0800

    Issue Form Array Value Getting

--- src/app/issue/createissue/createissue.component.ts
+++ src/app/issue/createissue/createissue.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, AbstractControl, FormArray } from '@angular/forms';
 import { AngularEditorConfig } from '@kolkov/angular-editor';
 
 @Component({
@@ -9,57 +9,71 @@ import { AngularEditorConfig } from '@kolkov/angular-editor';
   encapsulation: ViewEncapsulation.None
 })
 export class CreateissueComponent implements OnInit {
-issueForm:FormGroup;
-issueForm2:FormGroup;
-issueForm3:FormGroup;
+  issueForm: FormGroup;
+    
+  // Config for Text Editor
+  editorConfig: AngularEditorConfig = {
+    editable: true,
+    spellcheck: true,
+    height: '15rem',
+    minHeight: '5rem',
+    placeholder: 'Enter text here...',
+    translate: 'no',
+    defaultFontName: 'Arial',
+    customClasses: [
+      {
+        name: "quote",
+        class: "quote",
+      },
+      {
+        name: 'redText',
+        class: 'redText'
+      },
+      {
+        name: "titleText",
+        class: "titleText",
+        tag: "h1",
+      },
+    ],
+    toolbarPosition: 'top'
+  };
 
-// Config for Text Editor
-editorConfig: AngularEditorConfig = {
-  editable: true,
-  spellcheck: true,
-  height: '15rem',
-  minHeight: '5rem',
-  placeholder: 'Enter text here...',
-  translate: 'no',   
-  defaultFontName: 'Arial',
-  customClasses: [
-    {
-      name: "quote",
-      class: "quote",
-    },
-    {
-      name: 'redText',
-      class: 'redText'
-    },
-    {
-      name: "titleText",
-      class: "titleText",
-      tag: "h1",
-    },
-  ],
-  toolbarPosition:'top'
-};
+  public editor;
 
-public editor;
-
-  constructor(private fb:FormBuilder) { }
+  constructor(private _formBuilder: FormBuilder) { }
 
   ngOnInit() {
     this.buildForm();
   }
-  
-  buildForm()
-  {
-    this.issueForm = this.fb.group({
-      title:['',Validators.required],
-      description:['',Validators.required]
-    })
-    this.issueForm2 = this.fb.group({
-      screenshot:['',Validators.required]
-    })
-    this.issueForm3 = this.fb.group({
-      editorContent:['',Validators.required]
+  get formArray(): AbstractControl | null { return this.issueForm.get('issueFormArray'); }
+
+  buildForm() {
+
+    this.issueForm = this._formBuilder.group({
+      issueFormArray: this._formBuilder.array([
+        this._formBuilder.group({
+          title: ['', Validators.required],
+          description: ['', Validators.required]
+        }),
+        this._formBuilder.group({
+          screenshot: ['', Validators.required]
+        }),
+        this._formBuilder.group({
+          editorContent: ['']
+        })
+      ])
     })
   }
   
+  createIssue()
+  {
+    var issueDetails = this.issueForm.get('issueFormArray') as FormArray;
+    console.log(issueDetails.at(0).value.title);
+    console.log(this.issueForm.controls.title);
+    console.log(this.issueForm.value.description);
+    console.log(this.issueForm.value.screenshot);
+    console.log(this.issueForm.value.editorContent);
+    console.log(this.issueForm.value);
+    
+  }
 }
